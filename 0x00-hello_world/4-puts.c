9f42239afe4d88a8236570ab574a21c6bc001d31
RealsirMikel <okesolam@gmail.com>
2022-03-10 07:03:17 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
  /**
-  * main - prints the phrase "programming is like building a multilingual puzzle
-  * return : 0 if done the right way, otherwise non zero
+  * main - prints the quote
+  *
+  * Return: zero if done the right way, otherwise non zero
+  *
   */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle")
+	puts("\"programming is like building a multilingual puzzle");
 		return (0);
 }
