4692ac871ee183647b39f5f2f67a9ec9d3d2bea7
RealsirMikel <okesolam@gmail.com>
2022-03-13 05:10:53 -0700

    two numbers combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print combination of two sets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar(((a / 10) % 10) + '0');
+				putchar(a % 10 + '0');
+				putchar(' ');
+				putchar(((b / 10) % 10) + '0');
+				putchar(b % 10 + '0');
+				if (a != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
