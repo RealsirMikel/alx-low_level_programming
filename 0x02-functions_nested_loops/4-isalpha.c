42526c0955586b730e498205de975bb2bd18349b
RealsirMikel <okesolam@gmail.com>
2022-03-16 06:45:17 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * int _isalpha - prints alphabetic character
+ * _isalpha - prints alphabetic character
  * @c: expression
  * Return: Always 0 Success
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
