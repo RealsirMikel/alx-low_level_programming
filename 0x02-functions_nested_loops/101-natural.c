d8c69611ed6024d572a2b397299a8743f32575b3
RealsirMikel <okesolam@gmail.com>
2022-03-16 15:59:21 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - natural numbers
+ * Return: 0 Success
+ */
+
+int main (void)
+{
+	int a, b;
+
+	b = 0;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 5 == 0) || (a % 3 == 0))
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
