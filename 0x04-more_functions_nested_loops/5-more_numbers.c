68a5cf49f0613af62407c44c6defb8fcde640508
RealsirMikel <okesolam@gmail.com>
2022-03-18 04:54:49 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,6 +19,10 @@ void more_numbers(void)
 				_putchar((b / 10) % 10 + '0');
 				_putchar(b % 10 + '0');
 			}
+			else if (b < 9)
+			{
+				_putchar(b % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
