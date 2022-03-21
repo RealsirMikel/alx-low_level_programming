b675daf6d2fe9c2d1f1d0263b153fd7e633e56f3
RealsirMikel <okesolam@gmail.com>
2022-03-21 11:58:55 -0700

    pointer updating an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - connects integer to pointer and update the integer to 98
+ * @n: pointer
+ * Return: 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
